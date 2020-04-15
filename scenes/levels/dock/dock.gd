eda2f58803b121591c7b69fe620d3533ef388f76
Filip Volf <volffili@fit.cvut.cz>
2020-04-15 13:51:50 +0100

    add dock leave scene

--- scenes/levels/dock/dock.gd
+++ scenes/levels/dock/dock.gd
@@ -9,9 +9,3 @@ func _ready():
 	carousel.insert_text("sail")
 	carousel.insert_text("trade")
 	carousel.insert_text("town")
-	
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		Ship.port = Ship.port.destinations[carousel.selected]
-		Ship.days_to_dest = 5
-		get_tree().change_scene("res://scenes/levels/open_sea.tscn")
---
+++ scenes/levels/dock_leave/dock_leave.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+onready var carousel = $Carousel;
+
+func _ready():
+	carousel.state = "input"
+	for dest in Ship.port.destinations:
+		carousel.insert_text(dest.name_text)
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		Ship.port = Ship.port.destinations[carousel.selected]
+		Ship.days_to_dest = 5
+		get_tree().change_scene("res://scenes/levels/open_sea.tscn")
