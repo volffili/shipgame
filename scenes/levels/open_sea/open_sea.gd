4243a34c0ded499bb47d00e2d6a64199b9521d12
Matěj Hlaváček <matej.hl@gmail.com>
2020-04-16 22:25:59 +0200

    Create storm event

--- scenes/levels/open_sea/open_sea.gd
+++ scenes/levels/open_sea/open_sea.gd
@@ -7,8 +7,13 @@ func _process(delta):
 	elapsed_time += delta
 	if elapsed_time > 1:
 		elapsed_time -= 1
+		next_day()
 		
-		Calendar.next_day()
-		Ship.days_to_dest -= 1
-		if Ship.days_to_dest == 0:
-			get_tree().change_scene("res://scenes/levels/dock/dock.tscn")
+func next_day():
+	Calendar.next_day()
+	Ship.days_to_dest -= 1
+	if Ship.days_to_dest == 0:
+		get_tree().change_scene("res://scenes/levels/dock/dock.tscn")
+		
+	if randf() < 0.3:
+		get_tree().change_scene("res://scenes/levels/open_sea/events/storm.tscn")
