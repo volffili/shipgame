319c241e20e6de763751ff0c45d90e7f2e49b2c1
Matěj Hlaváček <matej.hl@gmail.com>
2020-04-16 21:26:24 +0200

    Improve carousel turning

--- scenes/subscenes/carousel/carousel.gd
+++ scenes/subscenes/carousel/carousel.gd
@@ -10,48 +10,37 @@ var radius := 128;
 var rot : float = 0;
 var target_angle : float = 0;
 var sin_multiplier := 0.5;
-var state := "input";
 var selected := 0
 
 func _ready():
 	set_process_input(true);
 
 func _input(event):
-	print(state, selected)
-	if(state != "input"):
-		return;
-		
 	if event.is_action_pressed("ui_accept"):
 		emit_signal("carousel_accept", items[selected].select_obj)
-	if event.is_action("ui_left"): 
+	if event.is_action_pressed("ui_left"): 
 		rotate_left();
-	if event.is_action("ui_right"): 
+	if event.is_action_pressed("ui_right"): 
 		rotate_right();
 
 func _process(delta):
-	match(state):
-		"interpolating":
-			print(target_angle, rot)
-			rot = Util.lerp_degrees(rot,target_angle,delta*10);
-			if (fmod(abs(target_angle-rot),2*PI) < 0.01):
-				rot = fmod(target_angle,2*PI);
-				state = "input";
-			_refresh_positions();
+	rot = Util.lerp_degrees(rot,target_angle,delta*10)
+	_refresh_positions()
 
 func _refresh_positions():
 	for i in range(items.size()):
-		var rot_i = rot+i*2*PI/items.size()+PI/2; 	
-		items[i].set_position(Vector2(cos(rot_i),sin(rot_i)*sin_multiplier)*radius);
+		var rot_i = rot+i*2*PI/items.size()+PI/2
+		items[i].set_position(Vector2(cos(rot_i),sin(rot_i)*sin_multiplier)*radius)
 
 func rotate_left():
-	state = "interpolating";
 	selected = (selected - 1) % items.size()
-	target_angle = rot + 2*PI/items.size()
+	target_angle = fmod(selected * 2*PI/items.size(), 2*PI)
+	rot -= 0.2 # this makes it rotate the right way with 2 items, also a nice ui feedback with 1 item
 	
 func rotate_right():
-	state = "interpolating";
 	selected = (selected + 1) % items.size()
-	target_angle = rot - 2*PI/items.size()
+	target_angle = fmod(selected * 2*PI/items.size(), 2*PI)
+	rot += 0.2
 	
 func insert(item, select_obj=null):
 	item.select_obj = select_obj
